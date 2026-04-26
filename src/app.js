427eef0ae64e1ee588dd495b6d5da46261d67f4c
Lagger Pablo <laggerpaglojose@gmail.com>
2026-04-26 20:03:59 -0300

    feat: scaffold product date model and base functions

---
+++ src/app.js
@@ -0,0 +1,33 @@
+// Estado inicial
+let productos = [];
+
+// Cargar datos desde LocalStorage
+function leerDeLS() {
+
+}
+
+// Guardar datos en LocalStorage
+function guardarEnLs() {
+
+}
+
+// Dibujar productos en la tabla
+function renderProductos() {
+
+}
+
+// Agregar nuevo producto
+function agregarProducto() {
+
+}
+
+// Actualizar stock de un producto 
+function actualizarStock() {
+
+}
+
+// Eliminar producto
+function eliminarProducto() {
+    
+}
+
